52388cc1f2b870191956bbf6f80674505bcd3d7d
jongseong <eello.bdev@gmail.com>
2023-03-17 17:26:01 +0900

    spring init

---
+++ be/spring/dodo/src/main/java/com/ssafy/dodo/DodoApplication.java
@@ -0,0 +1,15 @@
+package com.ssafy.dodo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.SpringBootConfiguration;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
+
+@SpringBootApplication(exclude = SecurityAutoConfiguration.class)
+public class DodoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DodoApplication.class, args);
+    }
+
+}
