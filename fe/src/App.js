fb1ecc40b085dea08b9f954ae8c93304b71386dd
SJPark97 <tjwlsdud33@naver.com>
2023-03-20 14:55:12 +0900

    feat: router설정

--- fe/src/App.js
+++ fe/src/App.js
@@ -1,26 +1,12 @@
-import logo from "./logo.svg";
 import "./App.css";
-import React, { useState } from "react";
+import { Routes, Route } from 'react-router-dom';
 
 function App() {
-  const [counter, setCounter] = useState(0);
-  const minus = () => {
-    setCounter(num => num - 1)
-  }
-  const plus = () => {
-    setCounter(num => num + 1)
-  }
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a className="App-link" href="https://reactjs.org" target="_blank" rel="noopener noreferrer">
-          Learn React
-        </a>
-      </header>
+      <Routes>
+        {/* <Route path="/" element={<Home />} /> */}
+      </Routes>
     </div>
   );
 }
