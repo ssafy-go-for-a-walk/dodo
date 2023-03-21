b6724abba98f6309bd1bf56cb04b755b5c8debc2
SJPark97 <tjwlsdud33@naver.com>
2023-03-21 15:19:17 +0900

    feat: 카카오 시작하기 페이지, 설문 페이지 크기 조정

--- fe/src/App.js
+++ fe/src/App.js
@@ -1,20 +1,22 @@
 import { Routes, Route } from "react-router-dom";
 import SearchPage from "./pages/SearchPage";
-import SurveyPage from "./pages/SurveyPage";
-import LoginPage from "./pages/LoginPage";
-import InfoPage from "./pages/InfoPage";
-import ManagePage from "./pages/ManagePage";
-import SocialPage from "./pages/SocialPage";
-import SharePage from "./pages/SharePage";
-import Page404 from "./pages/Page404";
+import SurveyPage from "./pages/Survey/SurveyPage";
+import SignUpPage from "./pages/Survey/SignUpPage";
+// import LoginPage from "./pages/LoginPage";
+// import InfoPage from "./pages/InfoPage";
+// import ManagePage from "./pages/ManagePage";
+// import SocialPage from "./pages/SocialPage";
+// import SharePage from "./pages/SharePage";
+// import Page404 from "./pages/Page404";
 
 function App() {
   return (
     <div>
       <Routes>
         <Route path="/" element={<SearchPage />} />
-        {/* <Route path="/survey" element={<SurveyPage />} />
-        <Route path="/login" element={<LoginPage />} />
+        <Route path="/survey" element={<SurveyPage />} />
+        <Route path="/survey/signup" element={<SignUpPage />} />
+        {/* <Route path="/login" element={<LoginPage />} />
         <Route path="/info" element={<InfoPage />} />
         <Route path="/manage/:id" element={<ManagePage />} />
         <Route path="/social" element={<SocialPage />} />
