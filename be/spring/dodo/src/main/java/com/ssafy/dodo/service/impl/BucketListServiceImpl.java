eb155fa26903e738b217e7d2ee2cdfb6795b1ed1
yeomyuri42@gmail.com <yeomyuri42@gmail.com>
2023-03-22 17:04:10 +0900

    fix: 버킷리스트 검사 추가

--- be/spring/dodo/src/main/java/com/ssafy/dodo/service/impl/BucketListServiceImpl.java
+++ be/spring/dodo/src/main/java/com/ssafy/dodo/service/impl/BucketListServiceImpl.java
@@ -2,8 +2,10 @@ package com.ssafy.dodo.service.impl;
 
 import com.ssafy.dodo.dto.AddedBucketDto;
 import com.ssafy.dodo.entity.AddedBucket;
+import com.ssafy.dodo.entity.BucketList;
 import com.ssafy.dodo.entity.User;
 import com.ssafy.dodo.repository.AddedBucketRepository;
+import com.ssafy.dodo.repository.BucketListRepository;
 import com.ssafy.dodo.repository.UserRepository;
 import com.ssafy.dodo.service.BucketListService;
 import lombok.RequiredArgsConstructor;
@@ -13,6 +15,7 @@ import org.springframework.data.domain.Pageable;
 import org.springframework.http.HttpStatus;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Service;
+import org.springframework.web.server.ResponseStatusException;
 
 import javax.transaction.Transactional;
 
@@ -24,12 +27,16 @@ public class BucketListServiceImpl implements BucketListService {
 
     private final AddedBucketRepository addedBucketRepository;
     private final UserRepository userRepository;
+    private final BucketListRepository bucketListRepository;
 
     @Override
     public Page<AddedBucketDto> getBucketListBuckets(UserDetails userDetails, Long bucketListSeq, Pageable pageable) {
 
         User user = userRepository.findById(Long.parseLong(userDetails.getUsername()))
-                .orElseThrow(() ->  new RuntimeException("user not found"));
+                .orElseThrow(() -> new ResponseStatusException(HttpStatus.NOT_FOUND));
+
+        BucketList bucketList = bucketListRepository.findById(bucketListSeq)
+                .orElseThrow(() -> new ResponseStatusException(HttpStatus.NOT_FOUND));
 
         Page<AddedBucket> allByBucketList = addedBucketRepository.findAllByBucketList(bucketListSeq, pageable);
 
