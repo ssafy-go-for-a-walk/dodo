72c3223d9137c946fd210c88405cf5f1c3c5d369
이희진 <oliqoeiilo@naver.com>
2023-03-24 11:10:26 +0900

    router 설정중

--- be/python/fastapi/app/main.py
+++ be/python/fastapi/app/main.py
@@ -1,17 +1,12 @@
-from fastapi import FastAPI, Depends
+from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
-from sqlalchemy.orm import Session
-from database import conn
-from models import Category
-
-from recomm import testt
+from routes import recommand
 
 app = FastAPI()
 
-engine = conn()
-
 origins = ["*"]
 
+# 미들웨어 정의
 app.add_middleware(
     CORSMiddleware,
     allow_origins=["*"],
@@ -20,18 +15,10 @@ app.add_middleware(
     allow_headers=["*"],
 )
 
+# 라우터 정의
+app.include_router(recommand.router)
 
 @app.get('/')
 def home():
 	return {"message": "Hello world!"}
 
-
-@app.get('/sessionTest')
-def session_test(db: Session = Depends(engine.get_session)): # Depends : api가 호출될 때마다 db 세션 생성, 사용 끝나면 자동 세션 해제
-	example = db.query(Category).all()
-	return example
-
-@app.get('/hello')
-def myname(db: Session = Depends(engine.get_session)):
-	data = db.query(Category).all()
-	testt(data)
