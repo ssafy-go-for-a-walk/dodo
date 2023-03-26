32d6e77f008d8e65712664abfc075c99be17f8ac
김종성 <>
2023-03-27 02:33:59 +0900

    feat: 버킷 별 경험일기 조회

--- be/spring/dodo/src/main/java/com/ssafy/dodo/repository/ExpDiaryImageRepository.java
+++ be/spring/dodo/src/main/java/com/ssafy/dodo/repository/ExpDiaryImageRepository.java
@@ -3,5 +3,9 @@ package com.ssafy.dodo.repository;
 import com.ssafy.dodo.entity.DiaryImage;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface ExpDiaryImageRepository extends JpaRepository<DiaryImage, Long> {
+
+    List<DiaryImage> findAllBySeqIn(List<Long> seq);
 }
--- be/spring/dodo/src/main/java/com/ssafy/dodo/service/impl/ExpDiaryServiceImpl.java
+++ be/spring/dodo/src/main/java/com/ssafy/dodo/service/impl/ExpDiaryServiceImpl.java
@@ -2,8 +2,8 @@ package com.ssafy.dodo.service.impl;
 
 import com.ssafy.dodo.dto.WriteExpDiaryDto;
 import com.ssafy.dodo.entity.AddedBucket;
-import com.ssafy.dodo.entity.ExpDiary;
 import com.ssafy.dodo.entity.DiaryImage;
+import com.ssafy.dodo.entity.ExpDiary;
 import com.ssafy.dodo.entity.User;
 import com.ssafy.dodo.exception.CustomException;
 import com.ssafy.dodo.exception.ErrorCode;
@@ -15,6 +15,7 @@ import com.ssafy.dodo.service.ExpDiaryService;
 import com.ssafy.dodo.service.S3FileService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.multipart.MultipartFile;
@@ -74,5 +75,22 @@ public class ExpDiaryServiceImpl implements ExpDiaryService {
         }
 
         return expDiary;
+    }
+
+    @Override
+    public List<ExpDiary> getExpDiaryByAddedBucket(Long userSeq, Long bucketSeq, Pageable pageable) {
+        // 버킷이 요청한 사람의 것인 맞는지 확인
+        User writer = userRepository.findById(userSeq)
+                .orElseThrow(() -> new CustomException(ErrorCode.USER_NOT_FOUND));
+
+        AddedBucket bucket = addedBucketRepository.findById(bucketSeq)
+                .orElseThrow(() -> new CustomException(ErrorCode.BUCKET_NOT_FOUND));
+
+        if (!writer.getSeq().equals(bucket.getCreatedBy())) {
+            throw new CustomException(ErrorCode.NOT_BUCKET_OWNER);
+        }
+
+        // 경험일기 조회
+        return expDiaryRepository.findAllByAddedBucket(bucket, pageable);
     }
 }
