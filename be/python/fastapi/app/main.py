7f22b03bfeb60ddcdca94d4aa7862d1dd59f1426
이희진 <oliqoeiilo@naver.com>
2023-03-27 07:42:56 +0900

    feat: cbf doing

--- be/python/fastapi/app/main.py
+++ be/python/fastapi/app/main.py
@@ -1,6 +1,10 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Depends
 from fastapi.middleware.cors import CORSMiddleware
+from sqlalchemy.orm import Session
 from routes import recommand
+from config import conn
+
+from database.schemas import Category_dto
 
 app = FastAPI()
 
@@ -15,10 +19,17 @@ app.add_middleware(
     allow_headers=["*"],
 )
 
+engine = conn()
+
 # 라우터 정의
 app.include_router(recommand.router)
 
 @app.get('/')
 def home():
 	return {"message": "Hello world!"}
+
+@app.get('/test', response_model=Category_dto)
+def session_test(db: Session = Depends(engine.get_session)):  # Depends : api가 호출될 때마다 db 세션 생성, 사용 끝나면 자동 세션 해제
+	example = recommand.session_test(db)
+	return example
 
--- be/python/fastapi/app/routes/recommand.py
+++ be/python/fastapi/app/routes/recommand.py
@@ -1,10 +1,16 @@
 import logging
 from fastapi import Depends, APIRouter, Header
-from sqlalchemy.orm import Session
+from sqlalchemy.orm import Session, join
+from sqlalchemy.sql import text
 from config import conn
-from database.models import Category, Preference
+from database.models import Category, Preference, PublicBucket
 from auth.auth_handler import decodeJWT
 from typing import Optional
+from konlpy.tag import Okt
+from sklearn.feature_extraction.text import TfidfVectorizer
+from sklearn.metrics.pairwise import linear_kernel
+import numpy as np
+import pandas as pd
 
 router = APIRouter()
 
@@ -14,22 +20,60 @@ engine = conn()
 logging.config.fileConfig("logging.conf", disable_existing_loggers=False)
 logger = logging.getLogger(__name__)
 
-@router.get('/test')
-def session_test(db: Session = Depends(engine.get_session)): # Depends : api가 호출될 때마다 db 세션 생성, 사용 끝나면 자동 세션 해제
+# @router.get('/test')
+def session_test(db):
 	example = db.query(Category).all()
 	return example
 
 # Preference - title을 활용한 CBF - 코사인 유사도 활용
 @router.get("/buckets")
-async def bucket_recommand(category: str,
+def bucket_recommand(category: str, skip: int = 0, limit: int = 10,
 		    db: Session = Depends(engine.get_session), 
-	        Authorization: Optional[str] = Header("None")):
-	data = db.query(Preference).filter()
+	      Authorization: Optional[str] = Header("None")):
 	
 	logger.info(category)
 	# logger.info(Authorization)
 	token = decodeJWT(Authorization.split(' ')[1])
-	logger.info(f"LOGIN 정보: {token['userSeq']}")
+	user_seq=token['userSeq']
+	logger.info(f"LOGIN 정보: {user_seq}")
+
+	# 유저가 몇명 이상이면 협업 필터링을 해야할까?
+	# 너무 느린데.. 카테고리 별로 나눠서 해보기
+
+	prefer_data = db.query(Preference).filter(Preference.user_seq == user_seq).all()
+	data = db.query(PublicBucket).filter(PublicBucket.is_public == 0).all()
+
+	# print(pre[1].publicBucket.title)
+	# print(pre[1].user.nickname)
+	# print(data[0].title)
 	
-    
-	return data # 나중에 추천된 목록 리턴하기
+	# okt = Okt()
+	# contents = list(map(lambda x: x.title, data))
+	
+	# # print(contents)
+
+	# vocab = []
+	# for content in contents:
+	# 	vocab += okt.nouns(content)
+
+	# # print(vocab)
+
+	# vocab = list(set(vocab))
+
+	# stop_words=['하기']
+	# vocab = list(filter(lambda x: x not in stop_words, vocab))
+	
+	# # print(vocab)
+
+	# tfidf = TfidfVectorizer()
+	# tfidf_matrix = tfidf.fit_transform(vocab)
+	# print(tfidf_matrix.shape)
+
+	# consine_sim = linear_kernel(tfidf_matrix, tfidf_matrix)
+
+	# print(consine_sim)
+	# print(consine_sim[::-1])
+
+	# indices = pd.Series(vocat)
+	
+	return data[0] # 나중에 추천된 목록 리턴하기
