741a3152380a724bde39499b78817a2e647dfb83
yeomyuri42@gmail.com <yeomyuri42@gmail.com>
2023-03-27 15:05:19 +0900

    fix: 버킷 담을 때 중복 체크

--- be/spring/dodo/src/main/java/com/ssafy/dodo/repository/AddedBucketRepository.java
+++ be/spring/dodo/src/main/java/com/ssafy/dodo/repository/AddedBucketRepository.java
@@ -2,26 +2,19 @@ package com.ssafy.dodo.repository;
 
 import com.ssafy.dodo.entity.AddedBucket;
 import com.ssafy.dodo.entity.BucketList;
-import org.hibernate.annotations.SQLDelete;
+import com.ssafy.dodo.entity.PublicBucket;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
-import org.springframework.data.repository.query.Param;
-
-import java.util.List;
-
 
 public interface AddedBucketRepository extends JpaRepository<AddedBucket, Long> {
 
     Page<AddedBucket> findAllByBucketList(BucketList bucketList, Pageable pageable);
-
-//    @Query("select ab from AddedBucket ab where ab.bucketList.seq = :bucketListSeq")
-//    List<AddedBucket> findAllByBucketList(@Param("bucketListSeq") Long bucketListSeq);
-
     @Modifying
     @Query("UPDATE AddedBucket ab SET ab.isDelete = true WHERE ab.bucketList = :bucketList")
     void deleteByBucketList(BucketList bucketList);
 
+    boolean existsByBucketListAndPublicBucket(BucketList bucketList, PublicBucket publicBucket);
 }
