c778276f94f120e252528167953c9a39a65cfff0
SJPark97 <tjwlsdud33@naver.com>
2023-03-27 16:30:52 +0900

    add: 설문조사 무한스크롤 추가

--- fe/src/App.js
+++ fe/src/App.js
@@ -2,7 +2,7 @@ import { Routes, Route } from "react-router-dom";
 import SearchPage from "./pages/SearchPage";
 import SurveyPage from "./pages/survey/SurveyPage";
 import SignUpPage from "./pages/survey/SignUpPage";
-// import LoginPage from "./pages/LoginPage";
+import LoginPage from "./pages/LoginPage";
 // import InfoPage from "./pages/InfoPage";
 import ManagePage from "./pages/ManagePage";
 // import SocialPage from "./pages/SocialPage";
@@ -23,8 +23,8 @@ function App() {
         <Route path="/survey" element={<SurveyPage />} />
         <Route path="/survey/signup" element={<SignUpPage />} />
         <Route path="/manage" element={<ManagePage />} />
-        {/* <Route path="/login" element={<LoginPage />} />
-        <Route path="/info" element={<InfoPage />} />
+        <Route path="/login" element={<LoginPage />} />
+        {/* <Route path="/info" element={<InfoPage />} />
         <Route path="/social" element={<SocialPage />} />
         <Route path="/share" element={<SharePage />} />
         <Route path="*" element={<Page404 />} /> */}
