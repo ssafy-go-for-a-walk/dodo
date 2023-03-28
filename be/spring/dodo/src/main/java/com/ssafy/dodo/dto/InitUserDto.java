16dc0969c21308e5df910bb4ae0612b47f8a881e
jongseong <eello.bdev@gmail.com>
2023-03-28 13:42:29 +0900

    fix: 수정

--- be/spring/dodo/src/main/java/com/ssafy/dodo/dto/InitUserDto.java
+++ be/spring/dodo/src/main/java/com/ssafy/dodo/dto/InitUserDto.java
@@ -2,11 +2,12 @@ package com.ssafy.dodo.dto;
 
 import lombok.Getter;
 
+import java.util.ArrayList;
 import java.util.List;
 
 @Getter
 public class InitUserDto {
 
     String nickname;
-    List<Long> preferenceBuckets;
+    List<Long> preferenceBuckets = new ArrayList<>();
 }
