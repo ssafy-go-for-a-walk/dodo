a12ba0e4adb0cc0e020e7d4d5fb68c9f508141a3
jongseong <eello.bdev@gmail.com>
2023-03-29 17:34:25 +0900

    feat: 버킷리스트에 초대 ing

---
+++ be/spring/dodo/src/main/java/com/ssafy/dodo/repository/InviteTokenRepository.java
@@ -0,0 +1,7 @@
+package com.ssafy.dodo.repository;
+
+import com.ssafy.dodo.entity.InviteToken;
+import org.springframework.data.repository.CrudRepository;
+
+public interface InviteTokenRepository extends CrudRepository<InviteToken, String> {
+}
