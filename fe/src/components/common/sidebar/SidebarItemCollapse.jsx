908bc0affd38243eb113466660d96a70ffa41ac6
이진욱 <dlwls9130@naver.com>
2023-03-31 10:52:20 +0900

    feat: 관리페이지 정보 수정, 버킷 완료 및 삭제

--- fe/src/components/common/sidebar/SidebarItemCollapse.jsx
+++ fe/src/components/common/sidebar/SidebarItemCollapse.jsx
@@ -1,8 +1,8 @@
 import { Collapse, List, ListItemButton, ListItemText, Typography } from "@mui/material";
 import { useState } from "react";
 import colorConfigs from "../../../configs/colorConfigs";
-import ExpandLessOutlinedIcon from '@mui/icons-material/ExpandLessOutlined';
-import ExpandMoreOutlinedIcon from '@mui/icons-material/ExpandMoreOutlined';
+import ExpandLessOutlinedIcon from "@mui/icons-material/ExpandLessOutlined";
+import ExpandMoreOutlinedIcon from "@mui/icons-material/ExpandMoreOutlined";
 import SidebarBuckitlists from "./SidebarBuckitlists";
 
 export default function SidebarItemCollapse(props) {
@@ -10,47 +10,33 @@ export default function SidebarItemCollapse(props) {
   const item = props.item;
   const type = props.type;
 
-  return (
-    item !== [] ? (
-      <>
-        <ListItemButton
-          onClick={() => setOpen(!open)}
-          sx={{
-            "&:hover": {
-              backgroundColor: colorConfigs.sidebar.hoverBg
-            },
-            paddingY: "8px",
-            paddingX: "24px"
-          }}
-        >
-          {open ? <ExpandLessOutlinedIcon /> : <ExpandMoreOutlinedIcon />}
-          <ListItemText
-            disableTypography
-            primary={
-              <Typography sx={{fontWeight: "700"}}>
-                {type === "single" ? "나의 버킷리스트" : "그룹 버킷리스트"}
-              </Typography>
-            }
-            sx={{marginLeft: "30px"}}
-          />
-          <ListItemText
-            disableTypography
-            primary={
-              <Typography>
-                {item.length}
-              </Typography>
-            }
-            sx={{textAlign: "right"}}
-          />
-        </ListItemButton>
-        <Collapse in={open} timeout="auto">
-          <List>
-            {item.map((info, index) => (
-              <SidebarBuckitlists key={index} data={info}/>
-            ))}
-          </List>
-        </Collapse>
-      </>
-      ) : null
-  );
-};
+  return item !== [] ? (
+    <>
+      <ListItemButton
+        onClick={() => setOpen(!open)}
+        sx={{
+          "&:hover": {
+            backgroundColor: colorConfigs.sidebar.hoverBg,
+          },
+          paddingY: "8px",
+          paddingX: "24px",
+        }}
+      >
+        {open ? <ExpandLessOutlinedIcon /> : <ExpandMoreOutlinedIcon />}
+        <ListItemText
+          disableTypography
+          primary={<Typography sx={{ fontWeight: "700" }}>{type === "single" ? "나의 버킷리스트" : "그룹 버킷리스트"}</Typography>}
+          sx={{ marginLeft: "30px" }}
+        />
+        <ListItemText disableTypography primary={<Typography>{item.length}</Typography>} sx={{ textAlign: "right" }} />
+      </ListItemButton>
+      <Collapse in={open} timeout="auto">
+        <List>
+          {item.map((info, index) => (
+            <SidebarBuckitlists key={index} data={info} />
+          ))}
+        </List>
+      </Collapse>
+    </>
+  ) : null;
+}
