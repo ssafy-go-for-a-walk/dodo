cc1736f72977674d42c1df7f4638599b190bafb8
이희진 <oliqoeiilo@naver.com>
2023-03-31 09:22:32 +0900

    feat: 예외처리 추가

--- be/python/fastapi/app/routes/recommand.py
+++ be/python/fastapi/app/routes/recommand.py
@@ -72,8 +72,8 @@ def bucket_recommand_cbf(category: str = "전체", page: int = 0, size: int = 10
 			.all()
 	
 	logger.info(f"prefer_data 개수 : {len(prefer_data)}")
-	
-	if(len(pb_data) == 0 | len(prefer_data) == 0):
+
+	if(len(pb_data) == 0 or len(prefer_data) == 0):
 		raise HTTPException(status_code=400, detail="설문 조사를 하지 않은 유저는 추천이 불가합니다.")
 
 	# print(prefer_data[0].category_seq)
@@ -259,7 +259,7 @@ def user_recommand_cf(page: int = 0, size: int = 2,
 
 	logger.info(f"prefer_data 개수 : {len(prefer_data)}")
 
-	if(len(pb_data) == 0 | len(prefer_data) == 0):
+	if(len(pb_data) == 0 or len(prefer_data) == 0):
 		raise HTTPException(status_code=400, detail="설문 조사를 하지 않은 유저는 추천이 불가합니다.")
 
 
