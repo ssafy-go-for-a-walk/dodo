bba00f93d5f3216935630b19aab6e5991936e2d7
이진욱 <dlwls9130@naver.com>
2023-04-06 10:44:01 +0900

    fix: 공유페이지 수정

--- fe/src/pages/manage/ManageHeader.jsx
+++ fe/src/pages/manage/ManageHeader.jsx
@@ -4,7 +4,7 @@ import styled from "styled-components";
 
 const Div = styled.div`
   position: sticky;
-  top: 0;
+  top: 64px;
   width: 100%;
   height: 128px;
   border-bottom: 1px solid #757575;
